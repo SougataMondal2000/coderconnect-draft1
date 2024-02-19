5cfb5d80256f199db3a1d8664332b634c2bcaa96
Arpan Sen <sen.arpan12@gmail.com>
2024-02-20 01:27:04 +0530

    changes

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -8,13 +8,13 @@ const page = () => {
   return (
     <div className="h-[100vh]">
       <nav className="h-[10%] flex justify-between items-center px-40 py-5">
-        <h1 className="text-white text-xl font-bold">CoderConnect</h1>
+        <h1 className="text-[#14F713] text-xl font-bold">CoderConnect</h1>
         <Button label={"Log in"} bgColor={"white"} textColor={"black"}></Button>
       </nav>
       <section className="flex items-center justify-center h-[75%]">
         <div className="w-full flex justify-center items-center lg:px-32  p-5 flex-wrap lg:gap-28 md:gap-4 gap-28">
           <div className="lg:w-[600px] md:w-[400px]">
-            <h1 className="lg:text-5xl md:text-4xl text-3xl font-bold text-white">
+            <h1 className="lg:text-6xl md:text-4xl text-3xl font-bold text-white">
               Unleash your coding potential
               <span className="text-[#14F713]">!</span>
             </h1>
@@ -29,16 +29,19 @@ const page = () => {
               ></Button>
             </div>
           </div>
-          <div className="object-cover relative">
-            {/* <div className="w-[350px] h-[350px] absolute bg-[#14F713] rounded-3xl"></div> */}
-            <Image
-              src={coding}
-              alt=""
-              width={350}
-              height={350}
-              className="rounded-3xl lg:w-[350px] lg:h-[350px] md:w-[275px] md:h-[275px] object-cover"
-            />
-          </div>
+          <div className="relative">
+  {/* Green Shadow */}
+  <div className="absolute bg-[#14F713] rounded-3xl shadow-md lg:w-[350px] lg:h-[350px] md:w-[275px] md:h-[275px]"></div>
+  
+  {/* Image */}
+  <Image
+    src={coding}
+    alt=""
+    width={350}
+    height={350}
+    className="rounded-3xl lg:w-[350px] lg:h-[350px] md:w-[275px] md:h-[275px] object-cover relative right-10 top-10"
+  />
+</div>
         </div>
       </section>
       <footer className="h-[15%] bg-[#14F713] text-black w-full fixed bottom-0 flex justify-center items-center">
